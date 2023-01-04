66322fdc5b383ed813233a97c9ec023331719891
Ruban Sahoo <rubansahoo1@gmail.com>
2023-01-04 23:51:52 +0530

    learn: Variables contd...

--- 02variables/main.go
+++ 02variables/main.go
@@ -6,4 +6,8 @@ func main() {
   var username string = "ruban"
   fmt.Println(username)
   fmt.Printf("Variable is of type: %T \n", username)
+
+  var isLoggedIn bool = true
+  fmt.Println(isLoggedIn)
+  fmt.Printf("Variable is of type: %T \n", isLoggedIn)
 }
