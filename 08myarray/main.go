6dbf55eb68ce1baf145cd39f3b378fa09be3433a
Ruban Sahoo <rubansahoo1@gmail.com>
2023-01-13 17:24:22 +0530

    learn: Array in golang

---
+++ 08myarray/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Welcome to array in golang")
+
+  var fruitList[4]string
+  fruitList[0] = "Apple"
+  fruitList[1] = "Tomato"
+  fruitList[3] = "Peach"
+
+  fmt.Println("Fruit list is:", fruitList)
+  fmt.Println("Length of frui is:", len(fruitList))
+
+  var vegList = [3]string{"potato", "beans", "mushroom"}
+  fmt.Println("Vegy list is:", vegList, "with length:", len(vegList))
+
+}
