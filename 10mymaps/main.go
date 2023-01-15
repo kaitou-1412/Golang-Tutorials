4f16f51a77dbdf21582e59e50aae7d0f26a9996a
Ruban Sahoo <rubansahoo1@gmail.com>
2023-01-15 22:33:12 +0530

    learn: Maps in golang

--- 10mymaps/main.go
+++ 10mymaps/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Maps in golang")
+	languages := make(map[string]string)
+	languages["JS"] = "Javascript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+	fmt.Println("List of all languages:", languages)
+	fmt.Println("JS shorts for:", languages["JS"])
+	delete(languages, "RB")
+	fmt.Println("List of all languages:", languages)
+	// loops are interesting
+	for key, value := range languages {
+		fmt.Printf("For key %v, value is %v\n", key, value)
+	}
+	for _, value := range languages {
+		fmt.Printf("For key v, value is %v\n", value)
+	}
 }
